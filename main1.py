9f2c5be868e4cf61e63fc0fba61571d228a5d68e
root <salman.ahmed@stormatics.tech>
2023-05-22 17:03:39 +0500

    checkpoint

--- main1.py
+++ main1.py
@@ -7,7 +7,7 @@ with open('config.json') as config_file:
 
 common_variables = config_data["common_variables"]
 primary_variables = config_data["primary_variables"]
-
+standby_variables = config_data["standby_variables"]
 
 # Extract required values
 ansible_host = primary_variables['_PG_SERVER_PRIMARY_IP']
@@ -40,3 +40,47 @@ for key, value in extra_vars.items():
 
 subprocess.run(command)
 
+
+
+
+
+
+
+
+
+
+
+
+
+for standby_var in standby_variables:
+
+    # Extract required values
+    ansible_host = primary_variables['_PG_SERVER_STANDBY_IP']
+    ansible_connection = 'ssh'
+    ansible_user = primary_variables['_PG_SERVER_STANDBY_USERNAME']
+    ansible_become_pass = primary_variables['_PG_SERVER_STANDBY_PSSWD']
+
+    # Define extra variables
+    extra_vars = {
+        "_PG_SERVER_STANDBY_PORT": primary_variables['_PG_SERVER_STANDBY_PORT'],
+        "_PG_PASSWORD": primary_variables['_PG_PASSWORD'],
+        "_PG_VERSION": common_variables['_PG_SERVER_VERSION'],
+        "_PG_CIRRUS_CONF_DIRECTORY": common_variables['_PG_CIRRUS_CONF_DIRECTORY'],
+        "_PG_SERVER_STANDBY_DATA_DIRECTORY": primary_variables['_PG_SERVER_STANDBY_DATA_DIRECTORY']
+    }
+
+    # Execute Ansible playbook
+    command = [
+        'ansible-playbook',
+        '-i', ansible_host + ',',
+        '-e', f'ansible_connection={ansible_connection}',
+        '-e', f'ansible_user={ansible_user}',
+        '-e', f'ansible_become_pass={ansible_become_pass}',
+        'playbook1.yml'
+    ]
+
+    # Extend command with extra variables
+    for key, value in extra_vars.items():
+        command.extend(['-e', f'{key}={value}'])
+
+    subprocess.run(command)
